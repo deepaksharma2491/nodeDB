27c8e254b760763e9728476206102adddd008ba1
Deepak Sharma <deepaksharma@Deepaks-MacBook-Pro.local>
2015-10-18 14:22:40 +0530

    changes

--- config/index.js
+++
@@ -1,9 +0,0 @@
-var config = 
-{
-  host     : 'frankly-staging.ce1zyhcvu8o4.ap-southeast-1.rds.amazonaws.com',
-  user     : 'apiuser',
-  password : 'agent47@frankly.me',
-  database : 'frankly'
-};
-
-module.exports = config;
